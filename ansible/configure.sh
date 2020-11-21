057aaf880e0e9055a628cd9f26d40d9f470f329c
Mike Branstein <mikeb@kizan.com>
2020-11-20 23:17:56 -0500

    update master configure script

--- ansible/configure.sh
+++ ansible/configure.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+sudo ansible-playbook local-setup.yml
 sudo ansible-playbook ssh.yml -i hosts.yml
 ansible-playbook prepare.yml -i hosts.yml
 ansible-playbook control-plane.yml -i hosts.yml
 ansible-playbook nodes.yml -i hosts.yml
+ansible-playbook validate-cluster.yml -i hosts.yml
